640e3de98c0d02066aa8c7909cd1bf368ede8655
manuelj555 <programador.manuel@gmail.com>
2018-09-04 18:19:27 -0500

    Mejorando la movilidad en la administracion

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace Manuel\Bundle\DevAccessBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Generator\UrlGenerator;
 
 /**
  * @author Manuel Aguirre <programador.manuel@gmail.com>
@@ -71,12 +72,33 @@ class DefaultController extends Controller
     {
         $this->checkRoles();
 
-        return $this->render('@DevAccess/Default/index.html.twig');
+        return $this->render('@DevAccess/Default/index.html.twig', [
+            'last_url' => $this->resolveReferer($request),
+        ]);
     }
 
     private function checkRoles()
     {
         $accessRoles = $this->getParameter('manuel.dev_access.security.roles');
         array_walk($accessRoles, [$this, 'denyAccessUnlessGranted']);
+    }
+
+    /**
+     * @param Request $request
+     * @return null|string|string[]
+     */
+    private function resolveReferer(Request $request)
+    {
+        $referer = $request->headers->get('referer');
+        $internalUrls = [
+            $this->generateUrl('manuel_dev_access_config', [], UrlGenerator::ABSOLUTE_URL),
+            $this->generateUrl('manuel_dev_access_start_access', [], UrlGenerator::ABSOLUTE_URL),
+            $this->generateUrl('manuel_dev_access_finish_access', [], UrlGenerator::ABSOLUTE_URL),
+            $this->generateUrl('manuel_dev_access_finish_all_access', [], UrlGenerator::ABSOLUTE_URL),
+        ];
+
+        if ($referer && !in_array($referer, $internalUrls)) {
+            return $referer;
+        }
     }
 }
